e650180e684e7ca54ece5d3354ea99a085838526
luu-jennifer <jenniferluu87@gmail.com>
2022-10-28 12:21:22 -0700

    deleted weatherAPI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
 import "../partials/_header.scss";
-// import Weather from "./Weather";
 
 const Header = () => {
   return (
     <header>
-      {/* <Weather /> */}
       <h1>Ingenious Thoughts 🧠</h1>
     </header>
   );
