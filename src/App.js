2ecaecdefbb651d569d3c30a98308932a9c009ac
luu-jennifer <jenniferluu87@gmail.com>
2022-10-28 07:52:56 -0700

    deleted Weather API code

--- src/App.js
+++ src/App.js
@@ -19,5 +19,4 @@ export default App;
 // TODO:
 // 1. Update meta data and favicon. Figure out manifest.json and robots.txt
 // 2. Axios is already installed. Use it to fetch data from the API and display it on the page. Either images, giphy or reddit thoughts
-// 3. Add a button to the About component that will toggle the About component to display or not display
-// 4. Style all the buttons in tyopgraphy.scss and delete button styles from all other partials
+// 3. Style all the buttons in tyopgraphy.scss and delete button styles from all other partials
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import "../partials/_header.scss";
 import About from "./About";
+import Weather from "./Weather";
 
 const Header = () => {
   return (
     <header>
+      <Weather />
       <h1>🧐🤔 Ingenious Thoughts 🧠💭</h1>
       <About />
     </header>
