1ce68630d779a94e0bfd2678e6221e4f78423c90
luu-jennifer <jenniferluu87@gmail.com>
2022-10-29 14:17:18 -0700

    firebase updated to pushObjects

--- src/firebase.js
+++ src/firebase.js
@@ -19,17 +19,17 @@ import { initializeApp } from "firebase/app";
 
 
 //************************************************************************************************
-// pushObjectThoughts firebase project configuration
+// pushObjects firebase project configuration
 //************************************************************************************************
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDauzFa_K7w_bEzVt3GTN2w2BsiO-6ItMU",
-  authDomain: "ingenious-thoughts.firebaseapp.com",
-  databaseURL: "https://ingenious-thoughts-default-rtdb.firebaseio.com",
-  projectId: "ingenious-thoughts",
-  storageBucket: "ingenious-thoughts.appspot.com",
-  messagingSenderId: "686011475696",
-  appId: "1:686011475696:web:8234f7d41050f5c4bcc435"
+  apiKey: "AIzaSyCgFXFreKWJRbDA2Ejfyj3EWr34q8QZU3A",
+  authDomain: "pushobjects.firebaseapp.com",
+  databaseURL: "https://pushobjects-default-rtdb.firebaseio.com",
+  projectId: "pushobjects",
+  storageBucket: "pushobjects.appspot.com",
+  messagingSenderId: "418374552753",
+  appId: "1:418374552753:web:99480fff48d07aa8532690"
 };
 
 // Initialize Firebase
