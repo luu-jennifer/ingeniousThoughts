72529acda0a1f184db512738a3abb2762181f99f
luu-jennifer <jenniferluu87@gmail.com>
2022-10-29 17:19:08 -0700

    disabled weather API

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import "../partials/_header.scss";
-import Weather from "./apis/Weather";
+// import Weather from "./apis/Weather";
 
 const Header = () => {
   return (
     <header>
       <h1>I have thoughts... 💭</h1>
-      <Weather />
+      {/* <Weather /> */}
     </header>
   );
 }
